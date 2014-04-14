774a98ed7d37d80dedd2e89a303b5f705ff5b754
Patrick Jones <pmichaeljones@gmail.com>
2014-04-14 15:01:04 -0600

    Added superclass

--- CarClass.rb
+++ CarClass.rb
@@ -1,16 +1,10 @@
-class MyCar
-  attr_accessor :year, :color, :model, :current_speed
-
-  def initialize(year, color, model)
-    self.year = year
-    self.color = color
-    self.model = model
-    @current_speed = 0
+class Vehicle
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles/gallons} per mile is your mileage."
   end
 
   def speed_up(amount)
     self.current_speed += amount
-
     puts "The car is traveling #{self.current_speed} MPH."
   end
 
@@ -20,17 +14,30 @@ class MyCar
       if self.current_speed < 0
         self.current_speed = 0
       end
-
       puts "The car is now going #{self.current_speed} MPH."
     end
-
-    puts "The car is already stopped."
-
   end
 
   def shut_off
     self.current_speed = 0
   end
 
+  def spray_paint(color)
+    self.color = color
+  end
+
+end
+
+class MyCar < Vehicle
+
+  attr_accessor :color, :model, :current_speed
+  attr_reader :year
+
+  def initialize(year, color, model)
+    @year = year
+    self.color = color
+    self.model = model
+    @current_speed = 0
+  end
 
 end
