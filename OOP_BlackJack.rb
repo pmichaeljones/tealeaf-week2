bc336e617165efb7fb23d61ec3445d9900bbdbeb
Patrick Jones <pmichaeljones@gmail.com>
2014-04-18 11:09:15 -0600

    Partial game logic

--- OOP_BlackJack.rb
+++ OOP_BlackJack.rb
@@ -39,6 +39,39 @@
 
 # Step 4. Group Methods into Classes #
 
+###------START MODULES--------####
+
+
+module Scoring # This module is to be included in any class where a score needs to be evaluated
+
+  def evaluate_score(cards)
+    #an array of objects [<obj12345 @suit = 'Spade' @value = 'Ace'>, <ob12345 @suit = 'Club', @value = 'King'>]
+  total = 0
+  cards.each do |x|
+    if x.value == "Ace"
+      total += 11
+    elsif x.value.to_i == 0 # J, Q, K
+      total += 10
+    else
+      total += x.value.to_i
+    end
+
+  end
+
+  #correct for Aces
+  cards.select{|e| e.value == "Ace"}.count.times do
+    total -= 10 if total > 21
+  end
+
+  total
+  end
+
+end
+
+
+
+###-----END MODULES---------###
+
 class Player
   attr_accessor :name
   attr_reader :score, :hand
@@ -119,8 +152,6 @@ class Deck
       @cards = @cards.shuffle
     end #ends innder DO
   end #ends outer DO
-
-
   end
 
 
@@ -134,50 +165,59 @@ end
 
 
 
-###------START MODULES--------####
-
-
-module Scoring # This module is to be included in any class where a score needs to be evaluated
-
-  def evaluate_score(cards)
-    #an array of objects [<obj12345 Spade Ace>, <ob12345 Club King>]
-  total = 0
-  cards.each do |x|
-    if x.value == "Ace"
-      total += 11
-    elsif x.value.to_i == 0 # J, Q, K
-      total += 10
-    else
-      total += x.value.to_i
-    end
-
-  end
-
-  #correct for Aces
-  cards.select{|e| e.value == "Ace"}.count.times do
-    total -= 10 if total > 21
-  end
-
-  total
-  end
-
-end
-
-
-###-----END MODULES---------###
-
-
-
-
 # -------------------------------------------------------------- #
 
 
 # Step 5. Create 'Game Engine' Class called BlackJack
 
 class BlackJack
+  attr_accessor :player, :deck, :dealer
   include Scoring
 
-   def hit_or_stay
+  def initialize
+    puts "Welcome. Let's get started. What's your name?"
+    name = gets.chomp
+    @player = Player.new("#{name}")
+    @dealer = Dealer.new()
+    @deck = Deck.new()
+    @player_cards = []
+    @dealer_cards = []
+  end
+
+  def play_game
+    puts "Welcome, #{self.player.name}. Here is your first cards"
+    card1  = self.deck.deal
+    @player_cards << card1
+    puts "You got a a #{card1.name}"
+    card2 = self.deck.deal
+    @dealer_cards << card2
+    puts "Dealer got a #{card2.name}"
+    card3  = self.deck.deal
+    @player_cards << card3
+    puts "You got a a #{card3.name}"
+    card4 = self.deck.deal
+    @dealer_cards << card4
+    puts "Dealer's last card is face down."
+
+    player_total = evaluate_score(@player_cards)
+    puts "Your total is #{player_total}"
+
+    while true
+      puts "Hit or Stay?"
+      response = gets.chomp.capitalize
+      if response == "Hit"
+        hit_or_stay
+      elsif response == "Stay"
+        ###dealer turn
+      else
+        puts "I didn't understand."
+      end
+    end
+
+
+  end
+
+  def hit_or_stay
 
   end
 
