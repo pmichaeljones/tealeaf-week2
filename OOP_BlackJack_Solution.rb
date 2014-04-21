9cd5aab50b1266cf6e0b389e339661a776533f3a
Patrick Jones <pmichaeljones@gmail.com>
2014-04-21 10:06:55 -0600

    Final Solution

---
+++ OOP_BlackJack_Solution.rb
@@ -0,0 +1,36 @@
+# Exact the primary nouns
+
+class Card
+  attr_accessor :suit, :face_value
+
+  def initialize(suit, face_value)
+    @suit = suit
+    @face_value = face_value
+  end
+
+
+end
+
+
+class Deck
+
+
+end
+
+
+class Player
+
+
+end
+
+
+class Dealer
+
+
+end
+
+
+class Hand
+
+
+end
